0d4e379613705d1c61c2f0917587fa94fa9a62c4
Sooyoung Yoon <ayoooo240@gmail.com>
2022-05-29 15:18:07 +0900

    ✨ posts 라우트 생성

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 const Router = require('koa-router');
+const posts = require('./posts');
+
 const api = new Router();
+
+api.use('/posts', posts.routes());
 
 api.get('/test', (ctx) => {
   ctx.body = 'test 성공';
