6bc167c6fb1bc0076b42dd73ff0f8945ad592a29
Sooyoung Yoon <ayoooo240@gmail.com>
2022-05-30 16:38:02 +0900

    ✨ ObjectId 검증 후 400번 상태코드 처리

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,4 +1,16 @@
+import mongoose from 'mongoose';
 import Post from '../../models/post';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request (500 말고)
+    return;
+  }
+  return next();
+}
 
 export const write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
@@ -15,7 +27,52 @@ export const write = async (ctx) => {
   }
 };
 
-export const list = (ctx) => {};
-export const read = (ctx) => {};
-export const remove = (ctx) => {};
-export const update = (ctx) => {};
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터가 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트 된 데이터를 반환
+      // false일 때는 업데이트되기 전의 데이터를 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
