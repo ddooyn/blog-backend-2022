a0f86b931fa8c7485036e2bb668d3bbd67323189
Sooyoung Yoon <ayoooo240@gmail.com>
2022-06-28 23:02:29 +0900

    ✨ 로그인 검증 미들웨어를 글쓰기, 수정, 삭제 라우트에 추가

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,14 +1,16 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
-posts.post('/', postsCtrl.write);
 posts.get('/', postsCtrl.list);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
 post.get('/:id', postsCtrl.read);
-post.delete('/:id', postsCtrl.remove);
-post.patch('/:id', postsCtrl.update);
+post.delete('/:id', checkLoggedIn, postsCtrl.remove);
+post.patch('/:id', checkLoggedIn, postsCtrl.update);
+
 post.use('/:id', postsCtrl.checkObjectId, post.routes())
 
 export default posts;
